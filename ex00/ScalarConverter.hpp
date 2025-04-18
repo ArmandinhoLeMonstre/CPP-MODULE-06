64ac715f3811e9ec30f8d44bd9aeb8acdb5ccdee
Armand Mititelu <armitite@fu-r3-p1.s19.be>
2025-04-18 15:29:25 +0200

    Headers

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 15:28:20 by armitite          #+#    #+#             */
+/*   Updated: 2025/04/18 15:28:34 by armitite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include <iostream>
-#include <cstdlib>  // Pour strtod, atoi
-#include <cmath>    // Pour floor, isnan (non-standard en C++98)
-#include <limits>   // Pour numeric_limits
-#include <iomanip>  // Pour setprecision
-#include <cctype>   // Pour isprint
+#include <cstdlib>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <cctype>
 
 class ScalarConverter {
 
