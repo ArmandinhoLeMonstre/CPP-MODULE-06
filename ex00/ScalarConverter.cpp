ef8bebc38e4f63d59e8700f18525c180fffc5ef8
Armand Mititelu <armitite@fu-r3-p1.s19.be>
2025-04-18 15:57:01 +0200

    Done

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 15:28:17 by armitite          #+#    #+#             */
-/*   Updated: 2025/04/18 15:28:18 by armitite         ###   ########.fr       */
+/*   Updated: 2025/04/18 15:54:55 by armitite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,16 @@ static void printFloat(double d) {
 }
 
 static void printDouble(double d) {
-    std::cout << "double: " << d;
 	if (std::isnan(d))
-        std::cout << "float: nan";
-    (d == static_cast<double>(static_cast<int>(d))) ? std::cout << ".0" : std::cout << "";
+        std::cout << "double: nan";
+    else
+    {    
+        std::cout << "double: " << d;
+        (d == static_cast<double>(static_cast<int>(d))) ? std::cout << ".0" : std::cout << "";
+    }
     std::cout << std::endl;
 }
 
-// --- Main Logic ---
 void ScalarConverter::convert(const std::string& s) {
     double d;
     char* endPtr;
@@ -68,16 +70,25 @@ void ScalarConverter::convert(const std::string& s) {
     }
 
     if (isPseudoLiteral(s)) {
-        d = (s == "nan" || s == "nanf") ? std::numeric_limits<double>::quiet_NaN() :
-            (s[0] == '-') ? -std::numeric_limits<double>::infinity() :
-            std::numeric_limits<double>::infinity();
+        if (s == "nan" || s == "nanf") {
+            d = std::numeric_limits<double>::quiet_NaN();
+        } 
+        else if (s[0] == '-') {
+            d = -std::numeric_limits<double>::infinity();
+        } 
+        else {
+            d = std::numeric_limits<double>::infinity();
+        }
     } 
     else if (s.length() == 1 && !isdigit(s[0])) {
         d = static_cast<double>(s[0]);
     }
     else {
         d = std::strtod(s.c_str(), &endPtr);
-        if ((*endPtr != '\0' && *endPtr != 'f') || (endPtr == s.c_str())) {
+        if (*endPtr == 'f') {
+            endPtr++;
+        }
+        if ((*endPtr != '\0')) {
             std::cout << "Error: invalid input format" << std::endl;
             return;
         }
